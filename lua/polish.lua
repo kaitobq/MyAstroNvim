7eae8b51c9ead16b60f3f30b6510fd22489452f8
kaitobq <kaitobq520@gmail.com>
2026-01-07 13:10:11 +0900

    bg

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,13 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Make Neovim background transparent (for terminal opacity like WezTerm).
+local hl = vim.api.nvim_set_hl
+hl(0, "Normal", { bg = "none" })
+hl(0, "NormalNC", { bg = "none" })
+hl(0, "SignColumn", { bg = "none" })
+hl(0, "EndOfBuffer", { bg = "none" })
+hl(0, "VertSplit", { bg = "none" })
+hl(0, "NormalFloat", { bg = "none" })
+hl(0, "FloatBorder", { bg = "none" })
